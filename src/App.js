d69f57102e2dd802db8db14589771dd1be967383
Zumrud Nerman <48273585+ZuuZaa@users.noreply.github.com>
2022-01-24 19:00:00 +0400

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Layout from "containers/MainLayout";
 
 function App() {
   return (
     <div className="App">
-
+      <Layout />
     </div>
   );
 }
