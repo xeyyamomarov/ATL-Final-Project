73a3957aaf6c54df2adc652d8822e3cda3e0494f
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-01-29 22:24:42 +0400

    duplicate Table deleted

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,106 +1,88 @@
-// import { useState } from 'react';
-// import Paper from '@mui/material/Paper';
-// import { Avatar, Table as ATable, Typography, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Box, } from '@mui/material/';
-// import { Edit, MoreHoriz } from '@mui/icons-material';
+import { useState } from 'react';
+import Paper from '@mui/material/Paper';
+import { Avatar, Table as ATable, Typography, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Box, } from '@mui/material/';
+import { Edit, MoreHoriz } from '@mui/icons-material';
 
-// export default function Table({ tbody = [], thead = [] }) {
-//   const [page, setPage] = useState(0);
-//   const [rowsPerPage, setRowsPerPage] = useState(10);
-//   const columns = [
-//     { id: 'fullName', label: 'Adı', minWidth: 220 },
-//     { id: 'username', label: 'Tipi', minWidth: 120 },
-//     {
-//       id: 'email',
-//       label: 'Elektron poçt',
-//       minWidth: 170,
-//     {
-//       id: 'position',
-//       label: 'Vəzifə',
-//       minWidth: 170,
-//     },
-//     {
-//       id: 'phone',
-//       label: 'Əlaqə nömrəsi',
-//       minWidth: 170,
-//     },
-//   ];
+export default function Table({ tbody = [], thead = [] }) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-//   const handleChangePage = (event, newPage) => {
-//     setPage(newPage);
-//   };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-//   const handleChangeRowsPerPage = (event) => {
-//     setRowsPerPage(+event.target.value);
-//     setPage(0);
-//   };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-//   return (
-//     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-//       <TableContainer sx={{ maxHeight: 302 }}>
-//         <ATable stickyHeader aria-label="sticky table">
-//           <TableHead>
-//             <TableRow>
-//               {thead.map((column) => (
-//                 <TableCell
-//                   key={column.id}
-//                   style={{ minWidth: column.minWidth, backgroundColor: '#F5F5F5' }}
-//                 >
-//                   {column.label}
-//                 </TableCell>
-//               ))}
-//               <TableCell
-//                 key='icons'
-//                 style={{ minWidth: '100px', backgroundColor: '#F5F5F5' }}
-//               >
-//               </TableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {tbody.map((row) => {
-//               return (
-//                 <TableRow hover role="checkbox" key={row.id}>
-//                   {thead.map((column) => {
-//                     const value = row[column.id];
-//                     return (
-//                       <TableCell key={column.id}>
-//                         <Box display='flex' alignItems='center'>
-//                           {column.id === "fullName" && <Avatar
-//                             alt={`${value}`}
-//                             src={row.avatar}
-//                             sx={{
-//                               width: 28, height: 28, display: 'flex', marginRight: '10px'
-//                             }}
-//                           />}
-//                           <Typography>
-//                             {value}
-//                           </Typography>
-//                         </Box>
-//                       </TableCell>
-//                     );
-//                   })}
-//                   <TableCell key='icons'
-//                     sx={{
-//                       textAlign: 'right',
-//                     }}
-//                   >
-//                     <Edit fontSize='small' />
-//                     <MoreHoriz sx={{ marginLeft: '10px', fontSize: 'medium' }} />
-//                   </TableCell>
-//                 </TableRow>
-//               );
-//             })}
-//           </TableBody>
-//         </ATable>
-//       </TableContainer>
-//       <TablePagination
-//         rowsPerPageOptions={[10, 25, 100]}
-//         component="div"
-//         count={tbody.length}
-//         rowsPerPage={rowsPerPage}
-//         page={page}
-//         onPageChange={handleChangePage}
-//         onRowsPerPageChange={handleChangeRowsPerPage}
-//       />
-//     </Paper>
-//   );
-// }
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 302 }}>
+        <ATable stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {thead.map((column) => (
+                <TableCell
+                  key={column.id}
+                  style={{ minWidth: column.minWidth, backgroundColor: '#F5F5F5' }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+              <TableCell
+                key='icons'
+                style={{ minWidth: '100px', backgroundColor: '#F5F5F5' }}
+              >
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tbody.map((row) => {
+              return (
+                <TableRow hover role="checkbox" key={row.id}>
+                  {thead.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id}>
+                        <Box display='flex' alignItems='center'>
+                          {column.id === "fullName" && <Avatar
+                            alt={`${value}`}
+                            src={row.avatar}
+                            sx={{
+                              width: 28, height: 28, display: 'flex', marginRight: '10px'
+                            }}
+                          />}
+                          <Typography>
+                            {value}
+                          </Typography>
+                        </Box>
+                      </TableCell>
+                    );
+                  })}
+                  <TableCell key='icons'
+                    sx={{
+                      textAlign: 'right',
+                    }}
+                  >
+                    <Edit fontSize='small' />
+                    <MoreHoriz sx={{ marginLeft: '10px', fontSize: 'medium' }} />
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </ATable>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={tbody.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
