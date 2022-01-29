d1459ebcc5b1606f07c7ee83302b2dfadd73feff
ELVINMAMMADLI <mamedli.elvin@gmail.com>
2022-01-29 18:43:23 +0400

    test

--- src/pages/Users/components/Dialogs/AddNewUserDialog.jsx
+++ src/pages/Users/components/Dialogs/AddNewUserDialog.jsx
@@ -1,112 +1,107 @@
-import { DialogContent, DialogTitle, Dialog, DialogActions, Box, Grid, TextField as MuiTextField } from "@mui/material";
-import { SubmitButton, CloseButton } from '../Buttons'
-import { Formik, Form, Field } from 'formik';
-import { Autocomplete, TextField } from 'formik-mui';
+import {
+  DialogContent,
+  DialogTitle,
+  Dialog,
+  DialogActions,
+  Box,
+  Grid,
+  TextField as MuiTextField,
+  Autocomplete
+} from "@mui/material";
+import { SubmitButton, CloseButton } from "../Buttons";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-mui";
 
 const positions = [
-  { title: "User", id: 1 }, { title: "Reporter", id : 2 }, { name: "Admin", id: 3 },{title: "Manager", id: 4}]
+  { title: "User", id: 1 },
+  { title: "Reporter", id: 2 },
+  { name: "Admin", id: 3 },
+  { title: "Manager", id: 4 },
+];
 
 const AddNewUserDialog = (props) => {
-
   const { open, close } = props;
 
   // const handleCreate = () => console.log(name, username, position, password, passwordRepeat)
-  const handleClose = () => close(prev => !prev);
-
+  const handleClose = () => close((prev) => !prev);
 
   return (
-
     <Dialog open={open}>
-
       <Formik
         initialValues={{
-          fullName: '',
-          username: '',
+          fullName: "",
+          username: "",
           position: [],
-          password: '',
-          passwordRepeat: '',
+          password: "",
+          passwordRepeat: "",
         }}
-        validate={(values) => {
-
-        }}
+        validate={(values) => {}}
         onSubmit={(values, { resetForm }) => {
-          console.log(values)
-          resetForm()
+          console.log(values);
+          resetForm();
         }}
       >
         {({ submitForm }) => (
           <Form>
-
-            <DialogTitle sx={{ padding: '12px 16px' }}>
+            <DialogTitle sx={{ padding: "12px 16px" }}>
               Yeni İstifadəçi
             </DialogTitle>
 
-            <DialogContent dividers sx={{ padding: '16px !important' }}>
-
+            <DialogContent dividers sx={{ padding: "16px !important" }}>
               <Box>
                 <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
+
+                  {/* <Grid item xs={12} sm={6}>
                     <Field
                       component={MuiTextField}
                       label="Əməkdaş*"
-                      name='fullName'
+                      name="fullName"
                     />
                   </Grid>
                   <Grid item xs={12} sm={6}>
                     <Field
                       component={MuiTextField}
                       label="İstifadəçi adı*"
-                      name='username'
+                      name="username"
                     />
-                  </Grid>
+                  </Grid> */}
 
                   <Grid item xs={12} sm={12}>
-
                     <Field
                       name="position"
                       component={Autocomplete}
                       options={positions}
-                      getOptionLabel={option => { console.log(option); return "" } }
+                      getOptionLabel={(option) => {
+                        console.log(option);
+                      }}
                       style={{ width: 300 }}
                       filterSelectedOptions
                       renderInput={(params) => {
-                        (
-                          <MuiTextField
-                            {...params}
-                            label="Rollar"
-                          />
-                        )
+                        <MuiTextField {...params} label="Rollar" />;
                       }}
                     />
+                  </Grid>
 
+                  {/* <Grid item xs={12} sm={6}>
+                    <Field component={MuiTextField} label="Şifrə*" />
                   </Grid>
 
                   <Grid item xs={12} sm={6}>
-                    <Field component={MuiTextField}
-                      label="Şifrə*"
-                    />
-                  </Grid>
-
-                  <Grid item xs={12} sm={6}>
-                    <Field component={MuiTextField}
-                      label="Şifrənin təkrarı*"
-                    />
-                  </Grid>
-
+                    <Field component={MuiTextField} label="Şifrənin təkrarı*" />
+                  </Grid> */}
                 </Grid>
               </Box>
             </DialogContent>
 
-            <DialogActions sx={{ padding: '12px 16px' }}>
+            <DialogActions sx={{ padding: "12px 16px" }}>
               <CloseButton onClick={handleClose} />
               <SubmitButton text="Yarat" onClick={submitForm} />
             </DialogActions>
-
           </Form>
         )}
       </Formik>
     </Dialog>
   );
-}
+};
 
 export default AddNewUserDialog;
