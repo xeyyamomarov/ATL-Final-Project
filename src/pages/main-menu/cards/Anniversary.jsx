a744e15a206fef4ca5b8d90061d1389a8e23bc78
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-01-30 22:03:45 +0400

    Table correction

--- src/pages/main-menu/cards/Anniversary.jsx
+++ src/pages/main-menu/cards/Anniversary.jsx
@@ -1,62 +1,62 @@
-import {TableBody,TableCell,Table as ATable,TableContainer,TableHead,TableRow,Paper,Card,CardContent,Typography,Tooltip,Box,Avatar} from '@mui/material';
+import { TableBody, TableCell, Table as ATable, TableContainer, TableHead, TableRow, Paper, Card, CardContent, Typography, Tooltip, Box, Avatar } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Table from 'components/Table/Table';
-import { useSelector ,useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getUsers } from 'store/Users/users.selectors';
 import { USERS_ACTIONS } from 'store/Users';
 
 
-  const style={
-    fontSize:"18px",
-    fontWeight:"bold",
-    width:"100px"
-  }
-const Anniversary=()=>{
+const style = {
+  fontSize: "18px",
+  fontWeight: "bold",
+  width: "100px"
+}
+const Anniversary = () => {
   const dispatch = useDispatch();
-//  const { datas, thead } = useSelector(getUsers);
-  const[isLoading,setIsLoading]=useState(false)
-  const [anniversary,setAnniversary] = useState({})
+  //  const { datas, thead } = useSelector(getUsers);
+  const [isLoading, setIsLoading] = useState(false)
+  const [anniversary, setAnniversary] = useState({})
 
 
-  useEffect(()=>{
+  useEffect(() => {
     axios(" http://localhost:3000/mainpage")
-    .then(data => {
-      setAnniversary(data.data.anniversary)
-      console.log(data.data.anniversary);
-    })
-    .catch(err=>console.log(err))
+      .then(data => {
+        setAnniversary(data.data.anniversary)
+        console.log(data.data.anniversary);
+      })
+      .catch(err => console.log(err))
 
-  },[isLoading])
+  }, [isLoading])
 
-  console.log(anniversary);
+  anniversary && console.log(anniversary);
 
- 
+
   // useEffect(() => {
   //   dispatch(USERS_ACTIONS.fetchCards())
   // }, [dispatch])
 
-    return(
-    
-      <Card sx={{marginBottom:2}}>
-       <Card sx={{backgroundColor:"#F5F5F5",boxShadow:"none"}}>
-        <CardContent sx={{display:"flex",justifyContent:"space-between",padding:0,height:"40px"}}>  
-        <Typography  variant='h6' position="relative" top="5px" left="15px" color="black" gutterBottom>
-         Qarşıdan gələn il dönümü
-        </Typography>
-        <Typography fontSize= "18px" position="relative" top="8px" right="15px"  gutterBottom>
-          <Tooltip title="Refresh">
-          <RefreshIcon sx={{cursor:"pointer"}} onClick={()=>{
-            setIsLoading(!isLoading)
-          }}/>
-          </Tooltip>
-        </Typography>
+  return (
+
+    <Card sx={{ marginBottom: 2 }}>
+      <Card sx={{ backgroundColor: "#F5F5F5", boxShadow: "none" }}>
+        <CardContent sx={{ display: "flex", justifyContent: "space-between", padding: 0, height: "40px" }}>
+          <Typography variant='h6' position="relative" top="5px" left="15px" color="black" gutterBottom>
+            Qarşıdan gələn il dönümü
+          </Typography>
+          <Typography fontSize="18px" position="relative" top="8px" right="15px" gutterBottom>
+            <Tooltip title="Refresh">
+              <RefreshIcon sx={{ cursor: "pointer" }} onClick={() => {
+                setIsLoading(!isLoading)
+              }} />
+            </Tooltip>
+          </Typography>
         </CardContent>
       </Card>
-      <Card sx={{ minWidth: 275 ,marginBottom:2,boxShadow:"none"}}>
-      <CardContent>
-      {/* <TableContainer component={Paper}>
+      <Card sx={{ minWidth: 275, marginBottom: 2, boxShadow: "none" }}>
+        <CardContent>
+          {/* <TableContainer component={Paper}>
     </TableContainer>
       <TableContainer component={Paper}>
 <ATable sx={{ minWidth: 650 }} aria-label="simple table">
@@ -90,11 +90,11 @@ const Anniversary=()=>{
   </TableBody>
 </ATable>
 </TableContainer> */}
- <Table tbody={anniversary.tbody} thead={anniversary.thead} />
-</CardContent>
-  </Card>
-  </Card>
+          <Table tbody={anniversary.tbody} thead={anniversary.thead} />
+        </CardContent>
+      </Card>
+    </Card>
 
-    )
+  )
 }
 export default Anniversary
