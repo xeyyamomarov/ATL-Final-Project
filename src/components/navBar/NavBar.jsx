bec6c6ceea4b78405397a9610edcbbcd1c43aa59
Zumrud Nerman <48273585+ZuuZaa@users.noreply.github.com>
2022-01-31 13:14:32 +0400

    navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,17 +1,39 @@
-import Box from "@mui/material/Box";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-const NavBar = () => {
-    return(
-        <Box
-        sx={{
-        width: 'auto',
-          height: 56,
-          backgroundColor: "red"
-        }}
-      >
-        Aydin Abbasov
-      </Box>
-    )
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: "#fff", color: 'black', height: 56, justifyContent:'center' }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Box >
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems:'center' }} >
+            <IconButton sx={{ p: 0, marginRight: 1 }}>
+              <Avatar alt="Avatar" src="" sx={{ width: 32, height: 32 }}/>
+            </IconButton>
+            <Typography component="div" sx={{ fontSize: 14 }}>
+              Aydin Abbasov
+            </Typography>
+          </Box>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default NavBar;
