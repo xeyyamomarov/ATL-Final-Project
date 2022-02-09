65be9c5352d1ca1ba08d87d157a36366874e3a92
Zumrud Nerman <48273585+ZuuZaa@users.noreply.github.com>
2022-02-09 15:51:41 +0400

    employees table

--- src/pages/EmployeesPage/Employees.jsx
+++ src/pages/EmployeesPage/Employees.jsx
@@ -1,9 +1,80 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { Table } from "components/Table";
+import { USERS_ACTIONS, USERS_SELECTORS } from "store/Users";
+//import { AddNewUserDialog, EditUserDialog, PasswordUpdateDialog } from "./components/Dialogs";
+//import { SearchBar } from "components/SearchBar";
+//import { SearchForm } from 'components/SearchForm';
+import { Collapse } from '@mui/material';
+//import { AddButton, SearchButton } from "components/Buttons";
+
 const EmployeesPage = () => {
+
+    const dispatch = useDispatch();
+    const { users } = useSelector(USERS_SELECTORS.getUsers);
+    const [searchOpen, setSearchOpen] = useState(false);
+    const thead = [
+        {
+            id: "fullName",
+            label: "A.S.A.",
+            minWidth: "220"
+        },
+        {
+            id: "username",
+            label: "İstifadəçi adı",
+            minWidth: "170"
+        },
+        {
+            id: "position",
+            label: "Vəzifə",
+            minWidth: "170"
+        },
+        {
+            id: "department",
+            label: "Şöbə",
+            minWidth: "170"
+        },
+        {
+            id: "email",
+            label: "Elektron poçt",
+            minWidth: "170"
+        },
+        {
+            id: "phone",
+            label: "Əlaqə nömrəsi",
+            minWidth: "170"
+        },
+        {
+            id: "icons",
+            label: "",
+            minWidth: "12px"
+        }
+    ]
+
+    useEffect(() => {
+        dispatch(USERS_ACTIONS.fetchUsers())
+    }, [dispatch])
+    console.log('employees:', users)
+
     return (
-        <section key="employees-page">
-            Employees Page
-        </section>
-      );
+        <>
+            {/* <SearchBar buttons={
+                <>
+                    <SearchButton onClick={() => {
+                        setSearchOpen(!searchOpen);
+                    }} />
+                    <AddButton onClick={() => {
+
+                    }} />
+                </>
+            } />
+            <Collapse in={searchOpen}>{<SearchForm />}</Collapse> */}
+            <Table thead={thead} tbody={users} pagination />
+            {/* <AddNewUserDialog />
+            <EditUserDialog />
+            <PasswordUpdateDialog /> */}
+        </>
+    );
 }
 
 export default EmployeesPage;
