50f2a11bb90e2c45e57ee399f9aad91075382062
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-05-16 00:11:28 +0400

    navbar sidebar re-created

---
+++ src/components/Sidebar/Styles.jsx
@@ -0,0 +1,31 @@
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles({
+  SidebarContainer: {
+    minHeight: "100vh",
+    padding: 10,
+    backgroundColor: "#9B5AE1"
+  },
+  logoWrapper: {
+    width: 180,
+    height: 16
+  },
+  navLinkIcon: { color: "#fff" },
+  navLinkText: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  arrow: {
+    color: "#fff",
+    marginLeft: 50
+  },
+  navLink: {
+    '&.active': {
+      backgroundColor: '#A66CE4',
+      borderRadius: 5
+    },
+    '&:hover': {
+      borderRadius: 5
+    }
+  }
+})
